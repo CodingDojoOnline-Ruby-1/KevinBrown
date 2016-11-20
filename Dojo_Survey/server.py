ab1f0cdd68d766c7125dbd0851c3d5976bd3df40
KBrow <Brown-sw@att.net>
2016-11-20 10:54:40 -0600

    Working Website Dojo Survey

--- Dojo_Survey/server.py
+++ Dojo_Survey/server.py
@@ -2,22 +2,15 @@ from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 
-ninjaInfo = {'name':"",
-    'dojoLocation':"",
-    'programmingLanguage':"",
-    'ninjaComment':"",
-}
-
 app.secret_key = "kavic84nses09bc03ncdald"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    session['ninjaInfo'] = 'ninjaInfo'
     return render_template('index.html')
+
 
 @app.route('/result', methods=['POST'])
 def result():
-    ninjaInfo = session.POST('ninjaInfo')
-    return render_template('result.html')
+    return render_template('result.html', form=request.form)
 
 app.run(debug = True)
