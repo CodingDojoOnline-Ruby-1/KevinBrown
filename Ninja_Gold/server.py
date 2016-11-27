57db166aff4c391a95b1a4ef5418e5ad279292af
KBrow <Brown-sw@att.net>
2016-11-27 05:59:08 -0600

    Working Code for Ninja Gold

--- Ninja_Gold/server.py
+++ Ninja_Gold/server.py
@@ -6,9 +6,10 @@ app.secret_key = "kavic84nses09bc03ncdald"
 
 @app.route('/')
 def index():
-    if not session.get('gold'):
+    if not session.get('activities'):
         session['gold'] = 0
-        session['activities'] = ''
+        print (str(session['gold']) +"<==========================")
+        session['activities'] = []
     return render_template('index.html')
 
 @app.route('/farmGold', methods = ['POST'])
@@ -16,7 +17,7 @@ def farmGold():
     newGold = random.randrange(10, 21)
     activity = "Worked on a farm and earned "+ str(newGold) +" gold."
     session['gold'] = session['gold'] + newGold
-    session['activities'] = activities.insert(0, activity)
+    session['activities'].insert(0, activity)
     return redirect('/')
 
 @app.route('/caveGold', methods = ['POST'])
@@ -24,22 +25,20 @@ def caveGold():
     newGold = random.randrange(5, 11)
     activity = "Explored a cave and found "+ str(newGold) +" gold."
     session['gold'] = session['gold'] + newGold
-    session['activities'] = activities.insert(0, activity)
+    session['activities'].insert(0, activity)
     return redirect('/')
 
 @app.route('/houseGold', methods = ['POST'])
 def houseGold():
     newGold = random.randrange(2, 6)
-    activities = session[activities]
     activity = "Robbed a house and stole "+ str(newGold) +" gold."
     session['gold'] = session['gold'] + newGold
-    activities.insert(0, activity)
-    session[activities] = activities
+    session['activities'].insert(0, activity)
     return redirect('/')
 
 @app.route('/casinoGold', methods = ['POST'])
 def casinoGold():
-    newGold = random.randrange(0, 52)
+    newGold = random.randrange(0, 53)
 
     if newGold == 0:
         newGold = ((session['gold']) * (-1))
@@ -54,7 +53,7 @@ def casinoGold():
         activity = "Went to a casino and won "+ str(newGold) +" gold."
 
     session['gold'] = session['gold'] + newGold
-    session['activities'] = activities.insert(0, activity)
+    session['activities'].insert(0, activity)
     return redirect('/')
 
 app.run(debug = True)
