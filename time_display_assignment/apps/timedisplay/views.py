13500dafb27f6a500a56a3ed58d19f54469cc3f1
KBrow <Brown-sw@att.net>
2016-12-15 06:53:59 -0600

    Working code without time, Time Display Assignment

--- time_display_assignment/apps/timedisplay/views.py
+++ time_display_assignment/apps/timedisplay/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render,
+from django.shortcuts import render
+
 import datetime
 
+
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
--- time_display_assignment/time_display_assignment/urls.py
+++ time_display_assignment/time_display_assignment/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include(apps.time_display.urls)),
+    url(r'^', include('apps.timedisplay.urls')),
+    # Update or include the new route here#
 ]
