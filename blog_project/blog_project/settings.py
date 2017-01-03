e5cd3dbeb3f36699910ce50f1d5154e9888c577d
KBrow <Brown-sw@att.net>
2017-01-03 07:30:54 -0600

    Updates and Corrections

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.blogs',
-    'django-extensions',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
