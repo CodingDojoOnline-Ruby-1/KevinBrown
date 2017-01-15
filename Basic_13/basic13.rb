702f4c4c5f0c1884f335047ae160aaf6c13cfc78
KBrow <Brown-sw@att.net>
2017-01-15 17:36:22 -0600

    Finished, cleaned-up and Refactored.

--- Basic_13/basic13.rb
+++ Basic_13/basic13.rb
@@ -6,7 +6,7 @@
 # Print 1-255 {#1}
 # Write a program (sets of instructions) that would print all the numbers from 1 to 255.
 # ================================================================================================================
-alfred = (1..255); alfred.each{|num| puts num}
+alfred = (1..255); puts; alfred.each{|num| puts num}
 # alfred = (1..255)
 # alfred.each{|num| puts num}
 
@@ -15,7 +15,7 @@ alfred = (1..255); alfred.each{|num| puts num}
 #Print odd numbers between 1-255 {#2}
 #Write a program (sets of instructions) that would print all the odd numbers from 1 to 255.
 # ================================================================================================================
-aman = (0..127); aman.each{|num| puts num * 2 + 1}
+aman = (0..127); puts; aman.each{|num| puts num * 2 + 1}
 # aman = (0..127)
 # aman.each{|num| puts num * 2 + 1}
 
@@ -30,7 +30,7 @@ aman = (0..127); aman.each{|num| puts num * 2 + 1}
 # New Number: 255 Sum: ___.
 #Do NOT use an array to do this exercise.
 # ================================================================================================================
-brandon = (0..255); brandon.each{|num1| puts "New Number: #{num1} Sum: #{y = (0..num1); sum =0; y.each{|num2| sum=sum+num2}; sum}"}
+brandon = (0..255); puts; brandon.each{|num1| puts "New Number: #{num1} Sum: #{y = (0..num1); sum =0; y.each{|num2| sum=sum+num2}; sum}"}
 # brandon = (0..255)
 # brandon.each{|num1| puts "New Number: #{num1} Sum: #{y = (0..num1); sum =0; y.each{|num2| sum=sum+num2}; sum}"}
 
@@ -39,7 +39,7 @@ brandon = (0..255); brandon.each{|num1| puts "New Number: #{num1} Sum: #{y = (0.
 # Iterating through an array {#4}
 # Given an array X, say [1,3,5,7,9,13], write a program that would iterate through each member of the array and print each value on the screen.  Being able to loop through each member of the array is extremely important.
 # ================================================================================================================
-carissa = [1, 3, 5, 7, 9, 13]; for i in 1..carissa.length; puts "Item " + (i).to_s + " is " + carissa[i-1].to_s; next i; end
+carissa = [1, 3, 5, 7, 9, 13]; puts; for i in 1..carissa.length; puts "Item " + (i).to_s + " is " + carissa[i-1].to_s; next i; end
 # carissa = [1, 3, 5, 7, 9, 13]
 # for i in 1..carissa.length
 #     puts "Item " + (i).to_s + " is " + carissa[i-1].to_s
@@ -51,7 +51,7 @@ carissa = [1, 3, 5, 7, 9, 13]; for i in 1..carissa.length; puts "Item " + (i).to
 # Find Max{#5}
 # Write a program (sets of instructions) that takes any array and prints the maximum value in the array. Your program should also work with a given array that has all negative numbers (e.g. [-3, -5, -7]), or even a mix of positive numbers, negative numbers and zero.
 # ================================================================================================================
-charlie = [-3, -5, -7, 0, 5, 13, -6, 4]; puts "The largest Number in the array is " + charlie.max.to_s
+charlie = [-3, -5, -7, 0, 5, 13, -6, 4]; puts; puts "The largest Number in the array is " + charlie.max.to_s
 # charlie = [-3, -5, -7, 0, 5, 13, -6, 4]
 # puts "The largest Number in the array is #{charlie.max}
 
@@ -60,7 +60,7 @@ charlie = [-3, -5, -7, 0, 5, 13, -6, 4]; puts "The largest Number in the array i
 # Get Average {#6}
 # Write a program that takes an array, and prints the AVERAGE of the values in the array. For example for an array [2, 10, 3], your program should print an average of 5. Again, make sure you come up with a simple base case and write instructions to solve that base case first, then test your instructions for other complicated cases. You can use a count function with this assignment.
 # ================================================================================================================
-francisco = [-3, -5, -7, 0, 5, 13, -6, 41, 3, 5, 7, 9, 14]; sum = 0.0; francisco.each_index{|i| sum += francisco[i]}; puts "The average is #{sum/francisco.count}."
+francisco = [-3, -5, -7, 0, 5, 13, -6, 41, 3, 5, 7, 9, 14]; puts; sum = 0.0; francisco.each_index{|i| sum += francisco[i]}; puts "The average is #{sum/francisco.count}."
 # francisco = [-3, -5, -7, 0, 5, 13, -6, 41, 3, 5, 7, 9, 14]
 # sum = 0.0
 # francisco.each_index{|i| sum += francisco[i]}; puts "The average is #{sum/francisco.count}."
@@ -70,7 +70,7 @@ francisco = [-3, -5, -7, 0, 5, 13, -6, 41, 3, 5, 7, 9, 14]; sum = 0.0; francisco
 # Array with Odd Numbers {#7}
 # Write a program that creates an array 'y' that contains all the odd numbers between 1 to 255. When the program is done, 'y' should have the value of [1, 3, 5, 7, ... 255].
 # ================================================================================================================
-y=[]; for i in 0..256; if i %2 == 1 then y.push(i) end; end; puts "The array 'y' is now: #{y}"
+y=[]; puts; for i in 0..256; if i %2 == 1 then y.push(i) end; end; puts "The array 'y' is now: #{y}"
 # y=[]
 # for i in 0..256
 #     if i %2 == 1 then
@@ -84,9 +84,10 @@ y=[]; for i in 0..256; if i %2 == 1 then y.push(i) end; end; puts "The array 'y'
 # Greater Than y {#8}
 # Write a program that takes an array and returns the number of values in that array whose value is greater than a given value y. For example, if array = [1, 3, 5, 7] and y = 3, after your program is run it will print 2 (since there are two values in the array that are greater than 3).
 # ================================================================================================================
-y = 3; gideon = [-3, -5, -7, 0, 5, 13, -6, 41, 3, 5, 7, 9, 14]; x = gideon.sort.dup; i = gl = gideon.length - 1; while i > -1 do; if x.at(i) > y then slicer = i; end; i = i - 1; end; w = x.slice(slicer..gl); puts "The array has #{w.count} numbers greater than y = #{y}."
+y = 3; gideon = [-3, -5, -7, 0, 5, 13, -6, 41, 3, 5, 7, 9, 14]; puts; x = gideon.sort.dup; i = gl = gideon.length - 1; while i > -1 do; if x.at(i) > y then slicer = i; end; i = i - 1; end; w = x.slice(slicer..gl); puts "The array has #{w.count} numbers greater than y = #{y}."
 # y = 3
 # gideon = [-3, -5, -7, 0, 5, 13, -6, 41, 3, 5, 7, 9, 14]
+# puts
 # x = gideon.sort.dup
 # i = gl = gideon.length - 1
 # while i > -1 do
@@ -166,15 +167,15 @@ x = [1, 5, 10, -2]; puts; puts "x is: #{x}"; i = x.length - 1; sum = 0.0; min = 
 # Shifting the values in the array {#12}
 # Given any array x, say [1, 5, 10, 7, -2], create an algorithm that shifts each number by one to the front.  For example, when the program is done, an x of [1, 5, 10, 7, -2] should become [5, 10, 7, -2, 0].
 # ================================================================================================================
+x = [1, 5, 10, 7, -2]; puts; puts "x is: #{x}"; for i in 0...x.length do; if x[i + 1] == nil then x[i] = 0; else x[i] = x[i + 1]; end; next i; end; puts "x is now: #{x}"
 # x = [1, 5, 10, 7, -2]
 # puts
 # puts "x is: #{x}"
-# puts x.length
-# for i in 0..x.length do
+# for i in 0...x.length do
 #     if x[i + 1] == nil then
 #         x[i] = 0
 #     else
-#         x[i] == x[i + 1]
+#         x[i] = x[i + 1]
 #     end #if
 #     next i
 # end #for
@@ -185,14 +186,15 @@ x = [1, 5, 10, -2]; puts; puts "x is: #{x}"; i = x.length - 1; sum = 0.0; min = 
 # Number to string {#13}
 # Write a program that takes an array of numbers and replaces any negative number with the string 'Dojo'.  For example, if array x is initially [-1, -3, 2] after your program is done that array should be ['Dojo', 'Dojo', 2].
 # ================================================================================================================
-# x = [1, 5, 10, -2]
+x = [-1, -3, 2]; puts; puts "x is: #{x}"; i = x.length - 1; while i > -1 do; if x[i] < 0 then x[i] = 'Dojo'; end; i = i - 1; end; puts "x is now: #{x}"
+# x = [-1, -3, 2]
+# puts
 # puts "x is: #{x}"
 # i = x.length - 1
 # while i > -1 do
 #     if x[i] < 0 then
-#         x[i] = 0
+#         x[i] = 'Dojo'
 #     end #if
 #     i = i - 1
 # end #while
 # puts "x is now: #{x}"
-# ================================================================================================================
