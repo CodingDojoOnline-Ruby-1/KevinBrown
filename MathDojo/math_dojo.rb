feac714367d60c2a6e361c70d02313f89cf3b63a
KBrow <Brown-sw@att.net>
2017-01-16 13:10:41 -0600

    Math Dojo Now working

--- MathDojo/math_dojo.rb
+++ MathDojo/math_dojo.rb
@@ -7,40 +7,39 @@
 #===============================================================================
 class MathDojo
 
-    def add *additives
-        sum = 0
-        for additive in additives do
-            puts "To #{sum} add #{additive} = "
-            sum = sum + additive
-        end #for
-        return self, sum
-    end #method
+    attr_reader :value
 
-    def subtract *subtractors
-        if subtractors.at(1) == nil then
-            return
-        elsif
-            sum =subtractors.at(0)
-            i = subtractors.length - 1
-            while i > 0 do
-                puts "To #{sum} subtract #{subtractors.at(i)} = "
-                sum = sum - subtractors.at(i)
-                i = i - 1
-            end #for
-        end #if
-        return self, sum
-    end #method
+    def initialize(value = 0)
+        @value = value
+        puts "Instance of #{self} initiated."
+    end
 
-    # def result sum
-    #     puts "The final value is #{sum}."
-    # end #method
+    def add *items
+        item = items.flatten.reduce(:+)
+        puts "To #{value} adding #{item}"
+        @value += item
+        return self
+    end #method-add
+
+    def subtract *items
+        item = items.flatten.reduce(:+)
+        puts "To #{value} subtracting #{item}"
+        @value -= item
+        return self
+    end #method-subtract
+
+    def result
+        puts "The value is #{@value}."
+    end #method
 
 end #class
 
-puts MathDojo.new.add(2).add(2, 5).subtract(3, 2)
-
+puts MathDojo.new.add(2).add(2, 5).subtract(3, 2).result()
+puts; puts;
 #===============================================================================
 # Modify MathDojo to take an array as a parameter with as many values passed into the array as needed.
 # (e.g. MathDojo.new.add(1).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract([2,3], [1.1, 2.3]).
 # result should do 0+1+(3+5+7+8)+(2+4.3+1.25)-(2+3)-(1.1+2.3) and return its result.
 #===============================================================================
+
+MathDojo.new.add(1).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract([2,3], [1.1, 2.3]).result()
