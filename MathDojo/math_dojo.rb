2f6937069fe8f678db333e856591295caf574af5
KBrow <Brown-sw@att.net>
2017-01-16 13:18:54 -0600

    Refactor

--- MathDojo/math_dojo.rb
+++ MathDojo/math_dojo.rb
@@ -1,10 +1,7 @@
 #_______________________________________________________________________________
 # Math Dojo Assignment
 #_______________________________________________________________________________
-# Develop a ruby class called MathDojo that has the following functions: add, subtract.
-# Have these 2 functions take at least 1 parameter.
-# MathDojo.new.add(2).add(2, 5).subtract(3, 2).result should perform 0+2+(2+5)-(3+2) and return 4.
-#===============================================================================
+
 class MathDojo
 
     attr_reader :value
@@ -34,12 +31,17 @@ class MathDojo
 
 end #class
 
+#===============================================================================
+# Develop a ruby class called MathDojo that has the following functions: add, subtract.
+# Have these 2 functions take at least 1 parameter.
+# MathDojo.new.add(2).add(2, 5).subtract(3, 2).result should perform 0+2+(2+5)-(3+2) and return 4.
+#===============================================================================
 puts MathDojo.new.add(2).add(2, 5).subtract(3, 2).result()
 puts; puts;
+
 #===============================================================================
 # Modify MathDojo to take an array as a parameter with as many values passed into the array as needed.
 # (e.g. MathDojo.new.add(1).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract([2,3], [1.1, 2.3]).
 # result should do 0+1+(3+5+7+8)+(2+4.3+1.25)-(2+3)-(1.1+2.3) and return its result.
 #===============================================================================
-
 MathDojo.new.add(1).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract([2,3], [1.1, 2.3]).result()
